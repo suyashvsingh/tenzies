4edc466467f4d86496acdebe184fa4143180ff6c
Aditya <adikpote@gmail.com>
2022-10-06 01:27:44 +0530

    Added Number of attempts in each round

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,12 +3,12 @@ import { useContext } from "react";
 import AppContext from "./context";
 
 function Button() {
-  const { onClick, over } = useContext(AppContext);
+  const { onClick, over ,count} = useContext(AppContext);
 
   return (
     <div className="button-area">
       <button className="btn" onClick={onClick}>{`${
-        over ? `You Won 🥳 Play Again` : `Roll`
+        over ? `You Won, you took ${count} attempts🥳 Play Again` : `Roll`
       }`}</button>
     </div>
   );
